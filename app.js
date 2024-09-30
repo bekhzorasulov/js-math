01879fbf24f685bd7dc4e80600ea56934b7292af
BekhzodRasulov <bekhzodr2001@gmail.com>
2024-09-30 18:44:45 +0500

    js math

--- app.js
+++ app.js
@@ -1,1 +1,81 @@
-let a = 12;
+// FOR misolar
+// 1 qisim
+
+// 1 misol
+// let n = prompt("son kiriting:");
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);
+// }
+
+// 2 misol
+// let n = prompt("son kiriting:");
+// for (let i = n; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// 3 misol
+// let n = prompt("son kiriting:");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+// 4 misol
+// let n = prompt("son kiriting:");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// 5 misol
+// let n = prompt("son kiriting:");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// let n = prompt("son kiriting:");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// 6 misol
+// let n = prompt("son kiriting:");
+// for (let i = 1; i <= n; i++) {
+//     if (n % i == 0) {
+//         console.log(i);
+//     }
+// }
+
+// 7 misol
+// let n = prompt("son kiriting:");
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     if (n % i === 0) {
+//         console.log(i);
+//         sum += i;
+//     }
+// }
+// console.log(sum);
+
+// 8 misol
+let n = prompt("son kiriting:");
+for (let i = 2; i <= n; i++) {
+    if (n % i === 0) {
+        console.log(i);
+    }
+}
