433bbd166658d31be4cc85c8d1192eb74369de03
BekhzodRasulov <bekhzodr2001@gmail.com>
2024-09-30 21:25:29 +0500

    js math

--- app.js
+++ app.js
@@ -73,9 +73,123 @@
 // console.log(sum);
 
 // 8 misol
-let n = prompt("son kiriting:");
-for (let i = 2; i <= n; i++) {
-    if (n % i === 0) {
-        console.log(i);
-    }
-}
+// let n = prompt("son kiriting:");
+// let sum = true;
+// if (n < 2) {
+//     sum = false;
+// } else {
+//     for (let i = 2; i < n; i++) {
+//         if (n % i == 0) {
+//             sum = false;
+//             break;
+//         }
+//     }
+// }
+// if (sum) {
+//     console.log("Yes");
+// } else {
+//     console.log("No");
+// }
+
+// FOR milolari
+// 2 qisim
+
+// 1 misol
+// let k = prompt("son kiriting:");
+// let n = prompt("son kiriting:");
+// for (i = 0; i < n; i++) {
+//     console.log(k);
+// }
+
+// 2 misol
+// let a = prompt("son kiriting:");
+// let b = prompt("son kiriting:");
+// let count = 0;
+// for (i = a; i <= b; i++) {
+//     console.log(i);
+//     count += 1;
+// }
+// console.log("Chiqarilgan sonlar sonni " + count);
+// console.log("Birinchi son " + a);
+// console.log("ikkinchi son " + b);
+
+// 3 misol
+// let a = prompt("son kiriting:");
+// let b = prompt("son kiriting:");
+// let count = 0;
+// for (let i = b - 1; i > a; i--) {
+//     console.log(i);
+//     count += 1;
+// }
+// console.log("Chiqarilgan sonlar sonni " + count);
+
+// 4 misol
+// let cost = prompt("1 kg konfet narxini kiriting:");
+// for (let i = 1; i <= 10; i++) {
+//     console.log(cost * i);
+// }
+
+// 5 misol
+// let cost = prompt("1 kg konfet narxini kiriting:");
+// let count = 0;
+// for (let i = 1; i <= 10; i++) {
+//     count = cost / 10 + count;
+//     console.log(count);
+// }
+
+// 6 misol
+// let cost = prompt("1 kg konfet narxini kiriting:");
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${(i / 10 + 1) * cost}`);
+// }
+
+// 7 misol
+// let a = prompt("son kiriting:");
+// let b = prompt("son kiriting:");
+// let count = 0;
+// for (let i = a; i <= b; i++) {
+//     count += Number(i);
+// }
+// console.log(count);
+
+// 8 misol
+// let a = prompt("son kiriting:");
+// let b = prompt("son kiriting:");
+// let count = 1;
+// for (let i = a; i <= b; i++) {
+//     count *= Number(i);
+// }
+// console.log(count);
+
+// 9 misol
+// let a = prompt("son kiriting:");
+// let b = prompt("son kiriting:");
+// let count = 0;
+// for (let i = a; i <= b; i++) {
+//     count += i * i;
+// }
+// console.log(count);
+
+// 10 misol
+// let a = prompt("son kiriting:");
+// let count = 0;
+// for (let i = 1; i <= a; i++) {
+//     count += 1 / i;
+// }
+// console.log(count);
+
+// 11 misol
+// let a = prompt("son kiriting:");
+// let count = 0;
+// for (let i = a; i <= 2 * a; i++) {
+//     count += i * i;
+// }
+// console.log(count);
+
+// 12 misol
+// let a = prompt("son kiriting:");
+// let count = 1;
+// for (let i = 1; i <= a; i++) {
+//     count *= 1 + i / 10;
+// }
+// console.log(count);
